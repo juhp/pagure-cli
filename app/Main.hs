e3f58a910419553620f3a6138188ae13d0985134
Jens Petersen <petersen@redhat.com>
2019-02-26 19:42:58 +0800

    Create the project

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main (main) where
+
+import Pagure (someFunc)
+
+
+main :: IO ()
+main = someFunc
---
+++ src/Pagure.hs
@@ -0,0 +1,6 @@
+module Pagure
+       ( someFunc
+       ) where
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
