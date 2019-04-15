5050f50de6f1fa68381533b4cc182fceef529412
Jens Petersen <petersen@redhat.com>
2019-04-15 23:26:06 +0800

    add --owner (default '!orphan') and --detail
    latest pagure supports 'owner=!orphan'

--- Main.hs
+++ Main.hs
@@ -8,27 +8,31 @@ import Lens.Micro
 import Lens.Micro.Aeson
 import SimpleCmdArgs
 
+import qualified Data.ByteString.Lazy.Char8 as B
+
 main :: IO ()
-main = do
+main =
   simpleCmdArgs Nothing "Pagure client" "Simple pagure CLI" $
     subcommands
     [Subcommand "list" "list projects" $
-     listProjects <$> strArg "PATTERN"]
+     listProjects <$> switchWith 'd' "detail" "Show all details" <*> strOptionalWith 'o' "owner" "OWNER" "Projects with certain owner" "!orphan" <*> strArg "PATTERN"]
     where
-      listProjects :: String -> IO ()
-      listProjects pat = do
+      listProjects :: Bool -> String -> String -> IO ()
+      listProjects detail owner pat = do
         mgr <- newManager tlsManagerSettings
-        let url = "https://src.fedoraproject.org/api/0/projects?namespace=rpms&fork=0&per_page=100&short=1&pattern=" ++ pat
+        let url = "https://src.fedoraproject.org/api/0/projects?namespace=rpms&fork=0&per_page=100" <> "&owner=" <> owner <> "&pattern=" <> pat
         req1 <- parseRequest url
         res1 <- responseBody <$> httpLbs req1 mgr
-        let pages = res1 ^? key "pagination" . key "pages" . _Integer
-        printProjects res1
-        mapM_ (nextPage mgr url) [2..(fromMaybe 0 pages)]
-          where
-            nextPage mgr url p = do
-              req <- parseRequest $ url ++ "&page=" ++ show p
-              res <- responseBody <$> httpLbs req mgr
-              printProjects res
+        if detail then B.putStrLn res1
+          else do
+          let pages = res1 ^? key "pagination" . key "pages" . _Integer
+          printProjects res1
+          mapM_ (nextPage mgr url) [2..(fromMaybe 0 pages)]
+            where
+              nextPage mgr url p = do
+                req <- parseRequest $ url <> "&page=" <> show p
+                res <- responseBody <$> httpLbs req mgr
+                printProjects res
 
-            printProjects res =
-              mapM_ T.putStrLn $ res ^.. key "projects" . values . key "name" . _String
+              printProjects res =
+                mapM_ T.putStrLn $ res ^.. key "projects" . values . key "name" . _String
