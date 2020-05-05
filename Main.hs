881a7356ba4befbcb778acd6354c87b928dc4ba1
Jens Petersen <petersen@redhat.com>
2020-05-05 17:25:16 +0800

    add --yaml (--json is no longer inline) and drop --url
    - use aeson-pretty for json output
    - no longer option to output api url
    - defaultPrinter for handling output choice
    - pagureQuery == Pagure.queryPagure

--- Main.hs
+++ Main.hs
@@ -14,7 +14,7 @@ import Control.Applicative (
 #endif
                            )
 import Control.Monad (unless, when)
-import Data.Aeson (eitherDecode)
+import Data.Aeson.Encode.Pretty (encodePretty)
 import Data.Aeson.Types
 #if (defined(MIN_VERSION_http_conduit) && MIN_VERSION_http_conduit(2,3,1))
 #else
@@ -38,7 +38,6 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Yaml (encode)
-import Network.HTTP.Conduit (queryString)
 import Network.HTTP.Simple
 import SimpleCmdArgs
 import System.FilePath ((</>))
@@ -51,31 +50,29 @@ main =
   simpleCmdArgs (Just version) "Pagure client" "Simple pagure CLI" $
   subcommands
   [ Subcommand "project" "show project details" $
-    projectInfo <$> serverOpt <*> strArg "PATTERN"
+    projectInfo <$> serverOpt <*> formatOpt <*> strArg "PATTERN"
   , Subcommand "list" "list projects" $
-    listProjects <$> serverOpt <*> countOpt <*> urlOpt <*> jsonOpt <*> forksOpt <*> optional namespaceOpt <*> optional packagerOpt <*> optional (strArg "PATTERN")
+    listProjects <$> serverOpt <*> countOpt <*> formatOpt <*> forksOpt <*> optional namespaceOpt <*> optional packagerOpt <*> optional (strArg "PATTERN")
   , Subcommand "user" "user repos" $
-    userRepos <$> serverOpt <*> countOpt <*> urlOpt <*> jsonOpt <*> strArg "USER"
+    userRepos <$> serverOpt <*> countOpt <*> formatOpt <*> strArg "USER"
   , Subcommand "branches" "list project branches" $
-    repoBranches <$> serverOpt <*> urlOpt <*> jsonOpt <*> strArg "REPO"
+    repoBranches <$> serverOpt <*> formatOpt <*> strArg "REPO"
   , Subcommand "issues" "list project issues" $
-    projectIssues <$> serverOpt <*> countOpt <*> urlOpt <*> jsonOpt <*> strArg "REPO" <*> switchWith 'A' "all" "list Open and Closed issues" <*> optional (strOptionWith 'a' "author" "AUTHOR" "Filter issues by creator") <*> optional (strOptionWith 'S' "since" "Y-M-D" "Filter issues updated after date") <*> optional (strOptionWith 't' "title" "pattern" "Filter issues by title")
+    projectIssues <$> serverOpt <*> countOpt <*> formatOpt <*> strArg "REPO" <*> switchWith 'A' "all" "list Open and Closed issues" <*> optional (strOptionWith 'a' "author" "AUTHOR" "Filter issues by creator") <*> optional (strOptionWith 'S' "since" "Y-M-D" "Filter issues updated after date") <*> optional (strOptionWith 't' "title" "pattern" "Filter issues by title")
   , Subcommand "issue" "show project issue" $
-    projectIssue <$> serverOpt <*> strArg "REPO" <*> argumentWith auto "ISSUE"
+    projectIssue <$> serverOpt <*> formatOpt <*> strArg "REPO" <*> argumentWith auto "ISSUE"
   , Subcommand "users" "list users" $
-    users <$> serverOpt <*> urlOpt <*> jsonOpt <*> strArg "PATTERN"
+    users <$> serverOpt <*> formatOpt <*> strArg "PATTERN"
   , Subcommand "username" "fullname of user" $
-    username <$> serverOpt <*> urlOpt <*> jsonOpt <*> strArg "USERNAME"
+    username <$> serverOpt <*> formatOpt <*> strArg "USERNAME"
   , Subcommand "groups" "list groups" $
-    groups <$> serverOpt <*> countOpt <*> urlOpt <*> jsonOpt <*> optional (strArg "PATTERN")
+    groups <$> serverOpt <*> countOpt <*> formatOpt <*> optional (strArg "PATTERN")
   , Subcommand "git-url" "show project repo's git urls" $
-    gitUrl <$> serverOpt <*> urlOpt <*> jsonOpt <*> strArg "REPO"
+    gitUrl <$> serverOpt <*> formatOpt <*> strArg "REPO"
   ]
   where
     countOpt = switchWith 'c' "count" "Show number only"
-    jsonOpt = switchWith 'j' "json" "Print raw json response"
-    -- FIXME should this be no-op?
-    urlOpt = switchWith 'U' "url" "Print API url"
+    formatOpt = flagWith' FormatJson 'j' "json" "Output JSON" <|> flagWith FormatDefault FormatYaml 'y' "yaml" "YAML output"
     namespaceOpt = strOptionWith 'n' "namespace" "NAMESPACE" "Specify project repo namespace"
     packagerOpt = Owner <$> ownerOpt <|> Committer <$> usernameOpt
     usernameOpt = strOptionWith 'u' "username" "USERNAME" "Projects to which username can commit"
@@ -83,6 +80,8 @@ main =
     serverOpt = strOptionalWith 's' "server" "SERVER" "Pagure server" srcFedoraprojectOrg
     forksOpt = flagWith' OnlyForks 'F' "only-forks" "Only list forks" <|>
                flagWith NoForks IncludeForks 'f' "include-forks" "Include forks [default: ignore forks]"
+
+data OutputFormat = FormatDefault | FormatJson | FormatYaml
 
 data Packager = Owner String | Committer String
 
@@ -96,15 +95,19 @@ srcFedoraprojectOrg = "src.fedoraproject.org"
 type Query = [(ByteString, Maybe ByteString)]
 #endif
 
-listProjects :: String -> Bool -> Bool -> Bool -> Forks -> Maybe String -> Maybe Packager -> Maybe String -> IO ()
-listProjects server count showurl json forks mnamespace mpackager mpattern = do
+defaultPrinter :: OutputFormat -> (Value -> IO ()) -> Value -> IO ()
+defaultPrinter FormatDefault pr = pr
+defaultPrinter FormatJson _ = BL.putStrLn . encodePretty
+defaultPrinter FormatYaml _ = B.putStrLn . encode
+
+listProjects :: String -> Bool -> OutputFormat -> Forks -> Maybe String -> Maybe Packager -> Maybe String -> IO ()
+listProjects server count format forks mnamespace mpackager mpattern = do
   unless (count || isJust mpackager || isJust mpattern) $
     error' "Please give a package pattern, --count, or --owner/--username"
   let path = "projects"
       params = makeKey "short" "1" ++ fork ++ packager ++ maybeKey "namespace" mnamespace ++ maybeKey "pattern" mpattern
-  pages <- queryPaged server count showurl json path params ("pagination", "page")
-  unless json $
-    mapM_ printPage pages
+  pages <- queryPaged server count path params ("pagination", "page")
+  mapM_ (defaultPrinter format printPage) pages
   where
     -- (!orphan only works on pagure >=0.29)
     packager = case mpackager of
@@ -122,12 +125,11 @@ listProjects server count showurl json forks mnamespace mpackager mpattern = do
       let key' = if isJust mnamespace then "name" else "fullname" in
       mapM_ T.putStrLn $ result ^.. key "projects" . values . key (T.pack key') . _String
 
-userRepos :: String -> Bool -> Bool -> Bool -> String -> IO ()
-userRepos server count showurl json user = do
+userRepos :: String -> Bool -> OutputFormat -> String -> IO ()
+userRepos server count format user = do
   let path = "user" </> user
-  pages <- queryPaged server count showurl json path [] ("repos_pagination", "repopage")
-  unless json $
-    mapM_ printPage pages
+  pages <- queryPaged server count path [] ("repos_pagination", "repopage")
+  mapM_ (defaultPrinter format printPage) pages
   where
     printPage :: Value -> IO ()
     printPage result = do
@@ -161,14 +163,13 @@ boolKey _ False _ = []
 boolKey k True val = makeKey k val
 
 -- FIXME limit max number of issues
-projectIssues :: String -> Bool -> Bool -> Bool -> String -> Bool -> Maybe String -> Maybe String -> Maybe String -> IO ()
-projectIssues server count showurl json repo allstatus mauthor msince mpat = do
+projectIssues :: String -> Bool -> OutputFormat -> String -> Bool -> Maybe String -> Maybe String -> Maybe String -> IO ()
+projectIssues server count format repo allstatus mauthor msince mpat = do
   let path = repo </> "issues"
       params = [("status", Just "all") | allstatus] ++
                maybeKey "author" mauthor ++ maybeKey "since" msince
-  results <- queryPaged server count showurl json path params ("pagination", "page")
-  unless json $
-    mapM_ printIssues results
+  pages <- queryPaged server count path params ("pagination", "page")
+  mapM_ (defaultPrinter format printIssues) pages
   where
     printIssues :: Value -> IO ()
     printIssues result = do
@@ -193,9 +194,9 @@ projectIssues server count showurl json repo allstatus mauthor msince mpat = do
         return (id',title,status)
 
 -- FIXME limit max number of pages (10?) or --pages
-queryPaged :: String -> Bool -> Bool -> Bool -> String -> Query -> (String,String) -> IO [Value]
-queryPaged server count showurl json path params (pagination,paging) = do
-  res1 <- pagureQuery showurl server json path (params ++ makeKey "per_page" (if count then "1" else "100"))
+queryPaged :: String -> Bool -> String -> Query -> (String,String) -> IO [Value]
+queryPaged server count path params (pagination,paging) = do
+  res1 <- pagureQuery server path (params ++ makeKey "per_page" (if count then "1" else "100"))
   let mpages = res1 ^? key (T.pack pagination) . key "pages" . _Integer
   if count
     then do
@@ -206,69 +207,60 @@ queryPaged server count showurl json path params (pagination,paging) = do
     return $ res1 : rest
       where
         nextPage p =
-          pagureQuery False server json path (params ++ makeKey "per_page" "100" ++ makeKey paging (show p))
+          pagureQuery server path (params ++ makeKey "per_page" "100" ++ makeKey paging (show p))
 
-pagureQuery :: Bool -> String -> Bool -> String -> Query -> IO Value
-pagureQuery showurl server json path params = do
+pagureQuery :: String -> String -> Query -> IO Value
+pagureQuery server path params = do
   let url = "https://" <> server </> "api/0" </> path
   req <- setRequestQueryString params <$> parseRequest url
-  when showurl $ putStrLn $ url ++ B.unpack (queryString req)
-  if json then do
-    res <- getResponseBody <$> httpLBS req
-    BL.putStrLn res
-    case eitherDecode res of
-      Left e -> error' e
-      Right v -> return v
-    else getResponseBody <$> httpJSON req
+  getResponseBody <$> httpJSON req
 
-repoBranches :: String -> Bool -> Bool -> String -> IO ()
-repoBranches server showurl json repo = do
+repoBranches :: String -> OutputFormat -> String -> IO ()
+repoBranches server format repo = do
   let namespace =
         if server == srcFedoraprojectOrg && '/' `notElem` repo
         then "rpms/" else ""
       path = namespace ++ repo </> "git/branches"
-  res <- pagureQuery showurl server json path []
-  unless json $
-    printKeyList "branches" res
+  res <- pagureQuery server path []
+  defaultPrinter format (printKeyList "branches") res
 
-users :: String -> Bool -> Bool -> String -> IO ()
-users server showurl json pat = do
+users :: String -> OutputFormat -> String -> IO ()
+users server format pat = do
   let path = "users"
       params = makeKey "pattern" pat
-  res <- pagureQuery showurl server json path params
-  unless json $
-    printKeyList "users" res
+  res <- pagureQuery server path params
+  defaultPrinter format (printKeyList "users") res
 
-username :: String -> Bool -> Bool -> String -> IO ()
-username server showurl json user = do
+username :: String -> OutputFormat -> String -> IO ()
+username server format user = do
   let path = "user" </> user
-  res <- pagureQuery showurl server json path $ makeKey "per_page" "1"
-  unless json $
-    case res ^? key "user" . key "fullname" . _String of
-      Nothing -> error' "User fullname not found"
-      Just fn -> T.putStrLn fn
+  res <- pagureQuery server path $ makeKey "per_page" "1"
+  defaultPrinter format printName res
+  where
+    printName res =
+      case res ^? key "user" . key "fullname" . _String of
+        Nothing -> error' "User fullname not found"
+        Just fn -> T.putStrLn fn
 
-groups :: String -> Bool -> Bool -> Bool -> Maybe String -> IO ()
-groups server count showurl json mpat = do
+groups :: String -> Bool -> OutputFormat -> Maybe String -> IO ()
+groups server count format mpat = do
   let path = "groups"
       params = maybeKey "pattern" mpat
-  results <- queryPaged server count showurl json path params ("pagination", "page")
-  unless json $
-    mapM_ (printKeyList "groups") results
+  pages <- queryPaged server count path params ("pagination", "page")
+  mapM_ (defaultPrinter format (printKeyList "groups")) pages
 
 printKeyList :: String -> Value -> IO ()
 printKeyList key' res =
   mapM_ T.putStrLn $ res ^.. key (T.pack key') . values . _String
 
-gitUrl :: String -> Bool -> Bool -> String -> IO ()
-gitUrl server showurl json repo = do
+gitUrl :: String -> OutputFormat -> String -> IO ()
+gitUrl server format repo = do
   let namespace =
         if server == srcFedoraprojectOrg && '/' `notElem` repo
         then "rpms/" else ""
       path = namespace ++ repo </> "git/urls"
-  res <- pagureQuery showurl server json path []
-  unless json $
-    printURLs res
+  res <- pagureQuery server path []
+  defaultPrinter format printURLs res
   where
     printURLs :: Value -> IO ()
     printURLs result =
@@ -282,12 +274,17 @@ error' = errorWithoutStackTrace
 error' = error
 #endif
 
-projectInfo :: String -> String -> IO ()
-projectInfo server repo = do
-  eval <- pagureProjectInfo server repo
-  either error' (B.putStrLn . encode) eval
+yamlPrinter :: OutputFormat -> Value -> IO ()
+yamlPrinter FormatDefault = yamlPrinter FormatYaml
+yamlPrinter FormatJson = BL.putStrLn . encodePretty
+yamlPrinter FormatYaml = B.putStrLn . encode
 
-projectIssue :: String -> String -> Int -> IO ()
-projectIssue server repo issue = do
+projectInfo :: String -> OutputFormat -> String -> IO ()
+projectInfo server format repo = do
+  eval <- pagureProjectInfo server repo
+  either error' (yamlPrinter format) eval
+
+projectIssue :: String -> OutputFormat -> String -> Int -> IO ()
+projectIssue server format repo issue = do
   eval <- pagureProjectIssueInfo server repo issue
-  either error' (B.putStrLn . encode) eval
+  either error' (yamlPrinter format . Object) eval
